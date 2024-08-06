e9d5961855b5cbbc9cb03142a334698fcf124991
tamilvanan <tamilvanansambasivam@gmail.com>
2024-08-06 14:43:09 +0530

    Add example for Parent to Child Communication

--- react-component-communication-examples/src/App.jsx
+++ react-component-communication-examples/src/App.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
+function Parent() {
+  const data = "hello from parent";
+
+  return <Children message={data} />;
+}
+
+function Children({ message }) {
+  return <div>{message}</div>;
+}
+
 export default function App() {
-  return <div>react-component-communication-examples</div>;
+  return (
+    <div>
+      <Parent />
+    </div>
+  );
 }
